2382014a2de0e06abe5ba255e603710be7f28ff3
Greg <gregeast94@gmail.com>
2025-06-21 00:12:58 -0400

    11-updates

--- 11-multiple-resources/compute.tf
+++ 11-multiple-resources/compute.tf
@@ -36,11 +36,11 @@ data "aws_ami" "nginx" {
   }
 }
 
-resource "aws_instance" "from_count" {
-  count         = var.ec2_instance_count
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main[count.index % length(aws_subnet.main)].id
+resource "aws_instance" "from_list" {
+  count         = length(var.ec2_instance_config_list)
+  ami           = local.ami_ids[var.ec2_instance_config_list[count.index].ami]
+  instance_type = var.ec2_instance_config_list[count.index].instance_type
+  subnet_id     = aws_subnet.main[var.ec2_instance_config_list[count.index].subnet_name].id
 
   tags = {
     Name    = "${local.project}-${count.index}"
@@ -48,14 +48,16 @@ resource "aws_instance" "from_count" {
   }
 }
 
-resource "aws_instance" "from_list" {
-  count         = length(var.ec2_instance_config_list)
-  ami           = local.ami_ids[var.ec2_instance_config_list[count.index].ami]
-  instance_type = var.ec2_instance_config_list[count.index].instance_type
-  subnet_id     = aws_subnet.main[count.index % length(aws_subnet.main)].id
+resource "aws_instance" "from_map" {
+  # each.key => holds the key of each key-value pair in the map
+  # each.value => holds the value each key-value pair in the map
+  for_each      = var.ec2_instance_config_map
+  ami           = local.ami_ids[each.value.ami]
+  instance_type = each.value.instance_type
+  subnet_id     = aws_subnet.main[each.value.subnet_name].id
 
   tags = {
-    Name    = "${local.project}-${count.index}"
+    Name    = "${local.project}-${each.key}"
     Project = local.project
   }
 }
